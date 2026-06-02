9820c92581dd093290b1fb0e7602141215505e5f
hector_pro <hectormartinez.pro@proton.me>
2026-06-02 15:25:45 -0700

    updated gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
-      <GoogleTagManager gtmId="G-T95CF8G9WQ" />
+      <GoogleTagManager gtmId="G-70YX9734M8" />
       <body>{children}</body>
     </html>
   );
