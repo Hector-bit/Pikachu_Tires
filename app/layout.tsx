0d8c29799e17c9ce85595a9e62e89d816f6248d4
hector_pro <hectormartinez.pro@proton.me>
2026-06-02 15:03:37 -0700

    added google tag to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
   title: "Pikachu's Tire Repair | Bellingham, WA",
@@ -9,6 +10,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
+      <GoogleTagManager gtmId="G-T95CF8G9WQ" />
       <body>{children}</body>
     </html>
   );
